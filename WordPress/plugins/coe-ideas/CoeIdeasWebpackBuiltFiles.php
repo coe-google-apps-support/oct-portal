3d9fde668c409869e6301170f44794bb53e650ff
Dan Chenier <daniel.chenier@edmonton.ca>
2018-01-15 11:25:35 -0700

    Updated coe-ideas WordPress Plugin

--- WordPress/plugins/coe-ideas/CoeIdeasWebpackBuiltFiles.php
+++ WordPress/plugins/coe-ideas/CoeIdeasWebpackBuiltFiles.php
@@ -2,10 +2,10 @@
 
 class CoeIdeasWebpackBuiltFiles {
   static $jsFiles = [
-    'js/vendor.faaf8cac1d889f47bc92.js',    'js/app.7d10f3673c65d78af2bd.js',    'js/manifest.ebc168f897bccc8c35d1.js',
+    'js/vendor.0744fa52f7f7c5144950.js',    'js/app.6839d422a6ce3404ef81.js',    'js/manifest.4545ecd58bc6339367a2.js',
   ];
   static $cssFiles = [
-    'css/app.07bcd8fec53937a8801852d484f61ed4.css',
+    'css/app.a1de3a6e4458cd8b30f64cc6f9994749.css',
   ];
 
 }
