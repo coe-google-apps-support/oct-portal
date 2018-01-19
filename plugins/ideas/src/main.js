01a83b9bac4123ec3666ce3c0528d1b5b55b48f5
j-rewerts <jaredrewerts@gmail.com>
2018-01-18 22:55:24 -0700

    Add in SCSS compiling.

--- plugins/ideas/src/main.js
+++ plugins/ideas/src/main.js
@@ -5,7 +5,6 @@ import AppIdeas from './AppIdeas'
 import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/black-green-light.css'
 import { HTTP } from './HttpCommon'
 import ServiceLoader from './services/service-loader.js'
 
