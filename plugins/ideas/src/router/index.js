1c59c8b7db99cd64fa2898942114b892b7846560
j-rewerts <jaredrewerts@gmail.com>
2018-01-22 14:46:29 -0700

    For now, route to a seperate page.

--- plugins/ideas/src/router/index.js
+++ plugins/ideas/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import NewIdea from '@/components/NewIdea'
 import ViewIdeas from '@/components/ViewIdeas'
+import ViewInitiative from '@/components/ViewInitiative'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/ViewIdeas',
       name: 'ViewIdeas',
       component: ViewIdeas
+    },
+    {
+      path: '/ViewIdeas/:id',
+      component: ViewInitiative,
+      props: true
     }
   ]
 })
