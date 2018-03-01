530d1cf16a94f756a7bf5d8df3760847e450af09
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-03-01 01:07:58 +0000

    Added an attach file button.
    Closes task 8010.
    https://cityofedmonton.visualstudio.com/Octava/_workitems/edit/8010

---
+++ plugins/ideas/src/components/AttachFileButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <md-button id="attach-file">
+          <google-doc-icon style="margin-bottom: 6px"></google-doc-icon>
+          <div v-if="!url">Attach</div>
+          <div v-else>Open</div>
+      </md-button>
+  </div>
+</template>
+
+<script>
+import GoogleDocIcon from '@/components/icons/GoogleDocIcon'
+
+export default {
+  name: 'AttachFileButton',
+  props: {
+    url: String
+  },
+  components: {
+    GoogleDocIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~vue-material/dist/theme/engine";
+
+  #attach-file {
+    height: 100px;
+    width: 100px;
+  }
+</style>
