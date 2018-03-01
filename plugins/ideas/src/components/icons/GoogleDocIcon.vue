dd1839c10345a707c492e593feabbbb9a55a7b53
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-03-01 23:32:46 +0000

    Added a weirdly ignored icon.

---
+++ plugins/ideas/src/components/icons/GoogleDocIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img width=64 height=64 v-if="imgSrc" :src="imgSrc"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GoogleDocIcon',
+    data: () => ({
+      imgSrc: null
+    }),
+    created: function () {
+      this.imgSrc = `${process.env.STATIC_ASSETS}/assets/icons/docs_64dp.png`
+    }
+  }
+</script>
