3e1ddad7af6889908469a52bf3646c0f8bb262f0
Dan Chenier <daniel.chenier@edmonton.ca>
2018-03-14 22:00:49 -0600

    Fixed Auditing (2)

---
+++ server/ideas-core/ideas-core/Migrations/20180315035810_Auditing.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoE.Ideas.Core.Migrations
+{
+    public partial class Auditing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddAuditing();
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RemoveAuditing();
+        }
+    }
+}
--- server/ideas-core/ideas-core/readme.md
+++ server/ideas-core/ideas-core/readme.md
@@ -1,2 +1,5 @@
 ﻿This class library forms the basis for all things related to initiatives on the server. 
 Contains classes for working with the database, service bus, WordPress, and the Initiaites web API.
+
+To update the database from the command line:
+dotnet ef database update --context InitiativeContext
