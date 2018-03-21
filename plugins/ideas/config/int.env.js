04cd7d6b183ec2ed27fad7ecbbb704a0e3dcc49f
Dan Chenier <daniel.chenier@edmonton.ca>
2018-03-21 12:10:31 -0600

    fixing dev

--- plugins/ideas/config/int.env.js
+++ plugins/ideas/config/int.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"integration"',
-  IDEAS_API: '"https://octavadev.edmonton.ca/api/initiatives"',
+  IDEAS_API: '"https://octavadev.edmonton.ca/plugins/initiatives/api"',
   STATIC_ASSETS: '"/wp-content/plugins/coe-ideas"'
 }
