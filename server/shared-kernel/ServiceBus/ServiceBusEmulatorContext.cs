0c83b2fa4073ca40145ba7fe8d3de5514a45f8e1
Dan Chenier <daniel.chenier@shaw.ca>
2018-04-16 10:59:10 -0600

    dotnet watch
    Updated shared-kernel and ideas-core to be compatible with dotnet watch

--- server/shared-kernel/ServiceBus/ServiceBusEmulatorContext.cs
+++ server/shared-kernel/ServiceBus/ServiceBusEmulatorContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace CoE.Ideas.Shared.ServiceBus
 {
-    internal class ServiceBusEmulatorContext : DbContext, IDesignTimeDbContextFactory<ServiceBusEmulatorContext>
+    internal class ServiceBusEmulatorContext : DbContext
     {
         public ServiceBusEmulatorContext() : base() { }
 
@@ -18,20 +18,6 @@ namespace CoE.Ideas.Shared.ServiceBus
         public DbSet<Message> Messages { get; set; }
 
         public DbSet<MessageProperty> MessageProperties { get; set; }
-
-        public ServiceBusEmulatorContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<ServiceBusEmulatorContext>();
-            optionsBuilder.UseSqlServer("server=.;database=ServiceBusEmulator;Trusted_Connection=True;");
-            return new ServiceBusEmulatorContext(optionsBuilder.Options);
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("server=.;database=ServiceBusEmulator;Trusted_Connection=True;");
-
-            base.OnConfiguring(optionsBuilder);
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
