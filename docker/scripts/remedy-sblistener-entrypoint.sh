7a2c88ac567a5e5332205966dcd82a18742e7869
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-04-20 18:46:54 +0000

    sblistener now pulls secrets if available.

---
+++ docker/scripts/remedy-sblistener-entrypoint.sh
@@ -0,0 +1,8 @@
+if [ -f /etc/secrets/appsettings.json ]; then
+  cp /etc/secrets/appsettings.json /app
+  echo "appsettings.json config specified."
+else
+  echo "No appsettings.json config specified."
+fi
+
+dotnet ideas-integration-remedy-sblistener.dll
