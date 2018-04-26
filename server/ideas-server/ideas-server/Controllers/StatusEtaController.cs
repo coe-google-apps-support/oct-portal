5e7cf057d75442c227dc559bd67bb2530e5c9ef1
Dan Chenier <daniel.chenier@shaw.ca>
2018-04-25 22:01:09 -0600

    fix

--- server/ideas-server/ideas-server/Controllers/StatusEtaController.cs
+++ server/ideas-server/ideas-server/Controllers/StatusEtaController.cs
@@ -4,6 +4,7 @@ using EnsureThat;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
