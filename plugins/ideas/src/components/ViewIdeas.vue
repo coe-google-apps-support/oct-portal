d0204d09406ffc4ef99aa6ed457ad24c0d570bcd
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2018-05-09 14:19:31 -0600

    Implemented infinite scroll with button, and redirect to my profile on submit initiative

--- plugins/ideas/src/components/ViewIdeas.vue
+++ plugins/ideas/src/components/ViewIdeas.vue
@@ -1,25 +1,39 @@
 <template>
   <div>
     <transition name="fade">
-      <div v-if="ideas && ideas.length" class="md-layout md-alignment-top-center">
-        <initiative v-for="idea in ideas"
-          :key="idea.id" 
-          :initiative="idea"
-          class="md-layout-item md-size-20 md-medium-size-30 md-small-size-100">
-        </initiative>
+      <div>
+        <div v-if="currentCards && currentCards.length" class="md-layout md-alignment-top-center">
+          <initiative v-for="idea in currentCards"
+            :key="idea.id" 
+            :initiative="idea"
+            :isNewIdea='idea.id === newInitiative && filter === "mine"'
+            class="md-layout-item md-size-20 md-medium-size-30 md-small-size-100">
+          </initiative>
+        </div>
+        <div v-if="ideas[currentCards.length + 1] != null"><button id='loadMore' v-on:click='infiniteHandler'>Load More </button></div>
+        <infinite-loading @infinite="infiniteHandler">
+         <!--  -->
+          <span slot="no-more">
+            There are no more initiatives!
+          </span>
+        </infinite-loading> 
       </div>
-    </transition> 
+    </transition>
+    
   </div>    
 </template>
 
 <script>
 import Initiative from '@/components/initiative'
+import InfiniteLoading from 'vue-infinite-loading'
+// import axios from 'axios'
 import vue from 'vue'
 
 export default {
   name: 'ViewIdeas',
   props: {
-    filter: String
+    filter: String,
+    newInitiative: Number
   },
   data: () => ({
     ideas: [],
@@ -27,10 +41,13 @@ export default {
     showDialog: false,
     shownInitiative: null,
     activeStep: null,
-    shownSteps: null
+    shownSteps: null,
+    firstInit: null,
+    currentCards: []
   }),
   components: {
-    Initiative
+    Initiative,
+    InfiniteLoading
   },
   methods: {
     setLoading (initiative, state) {
@@ -46,16 +63,59 @@ export default {
           return this.ideas[i]
         }
       }
-
       return null
+    },
+    infiniteHandler ($state) {
+      // console.log('starting infinite handler')
+      const self = this
+      setTimeout(() => {
+        const temp = []
+        for (let i = self.currentCards.length; i <= self.currentCards.length + 19; i++) {
+          if (self.ideas[i] != null) {
+            temp.push(self.ideas[i])
+          }
+        }
+        self.currentCards = self.currentCards.concat(temp)
+        if ($state.loaded) {
+          $state.loaded()
+        }
+        if (self.currentCards.length === 0) {
+          // console.error('currentCards length is 0.')
+        } else {
+          // console.log('All good! currentCards length is not 0!')
+          // console.log(self.currentCards)
+        }
+        if (self.ideas[self.currentCards.length + 1] == null) {
+          if ($state.complete) {
+            $state.complete()
+          }
+        }
+      }, 1000)
+      // console.log('finishing infinite handler')
     }
   },
   created () {
+    console.log(this.newInitiative)
+    console.log(this.filter)
+    // if (this.newInitiative !== null && this.filter === 'mine') {
+    //   this.$toasted.show('Initiative successfully submitted!', {
+    //     theme: 'primary',
+    //     position: 'top-right',
+    //     icon: 'check_circle',
+    //     action: {
+    //       text: 'Close',
+    //       onClick: (e, toastObject) => {
+    //         toastObject.goAway(0)
+    //       }
+    //     }
+    //   })
+    // }
     this.ideas.splice(0, this.ideas.length)
 
     let initiativeFunction = null
     if (this.filter === 'mine') {
       initiativeFunction = this.services.ideas.getMyInitiatives
+      // console.log('Getting my inits')
     } else {
       initiativeFunction = this.services.ideas.getIdeas
     }
@@ -75,4 +135,60 @@ export default {
   .md-overlay.md-fixed.md-dialog-overlay {
     z-index: 9!important;
   }
+
+#loadMore {
+  position: absolute;
+  margin: auto;
+  width: 20%;
+  right: 50%;
+  padding: 10px;
+  transform: translate(50%, 10%);
+  border: 1px solid #338033;
+  border-radius: 4px;
+  overflow: hidden;
+  transition: .6s;
+}
+#loadMore:focus {
+  outline: none;
+}
+#loadMore:before {
+  content: '';
+  display: block;
+  position: absolute;
+  background: rgba(255,255,255,.5);
+  width: 60px;
+  height: 100%;
+  left: 0;
+  top: 0;
+  opacity: .5s;
+  filter: blur(30px);
+  transform: translateX(-130px) skewX(-25deg);
+}
+#loadMore:after {
+  content: '';
+  display: block;
+  position: absolute;
+  background: rgba(255,255,255,.2);
+  width: 30px;
+  height: 100%;
+  left: 30px;
+  top: 0;
+  opacity: 0;
+  filter: blur(30px);
+  transform: translate(-100px) scaleX(-15deg);
+}
+#loadMore:hover {
+  background: #338033;
+  cursor: pointer;
+}
+#loadMore:hover:before {
+  transform: translate(300px) skewX(-15deg);
+  opacity: .6;
+  transition: 1.5s;
+}
+#loadMore:hover:after {
+  transform: translate(300px) skewX(-15deg);
+  opacity: 1;
+  transition:.7s;
+}
 </style>
