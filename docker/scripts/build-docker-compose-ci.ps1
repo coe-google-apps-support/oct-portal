1a2ca25ae552cca58ba7443db5ad101e9d9e6e70
Dan Chenier <daniel.chenier@shaw.ca>
2018-05-15 07:17:21 -0600

    Building PR CI docker-compose file

--- docker/scripts/build-docker-compose-ci.ps1
+++ docker/scripts/build-docker-compose-ci.ps1
@@ -1,17 +1,33 @@
 #This PowerShell script runs on the Docker-Container-CI build agent, 
 # built by Dockerfile.build-server
 
-#Import the PSYaml module, installed on agent by the Dockerfile.build-server
+Write-Host "BuildId is $BUILD_BUILDID"
+Write-Host "Staging Directory is $BUILD_ARTIFACTSTAGINGDIRECTORY"
 
-Import-Module /var/PSYaml/
-function New-DockerCompose {
-  param (
-      [string]$BaseDockerComposePath,
-      [string]$version
-  )
+$od = Get-DockerComposeFile ./docker-compose.yml
+$od.ReplaceImage("initiatives-vue", "coeoctava.azurecr.io/initiatives-vue:dev-1.0.$BUILD_BUILDID")
+$od.ReplaceImage("initiatives-webapi", "coeoctava.azurecr.io/initiatives-webapi:v1.0.$BUILD_BUILDID")
+$od.ReplaceImage("nginx", "coeoctava.azurecr.io/nginx:v1.0.$BUILD_BUILDID")
+
+foreach ($svcName in $od.services.Keys)
+{
+  $svc = $od.services[$svcName];
+
+  #Remove all exposed ports
+  $svc.Remove("ports");
+
+  #Ensure unique names for the services
+  $od.services.Remove($svcName);
+  $od.services[$svcName + "_$BUILD_BUILDID"] = $svc
+
+  if ($svc.Contains("depends_on")) {
+    $dependencies = $svc["depends_on"];
+    foreach ($dependencyName in $dependencies.Keys) {
+      $dependency = $dependencies[$dependencyName];
+      $dependencies.Remove($dependency);
+      $dependencies[$dependencyName + "_$BUILD_BUILDID"] = $dependency;
+    }
+  }
 }
 
-$dockerCompose = Get-Content /vsts/agent/_work/1/s/docker-compose.yml | Out-String | ConvertFrom-Yaml
-
-
-$yaml.services.'initiatives-vue'.image = "coeoctava.azurecr.io/initiatives-vue:dev-1.0.35"
+$od > ./docker-compose.yml
