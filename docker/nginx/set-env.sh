615c5e8b31ee21f49f78781f79d0a15c73cd404e
Dan Chenier <daniel.chenier@shaw.ca>
2018-05-16 13:36:58 -0600

    testing

--- docker/nginx/set-env.sh
+++ docker/nginx/set-env.sh
@@ -16,7 +16,9 @@ if [[ -z "${OCTAVA_PORT// }" ]] ; then OCTAVA_PORT=80; fi
 DOMAIN_NAME="$(echo $DOMAIN_NAME | awk -F':' '{print $1}')"
 
 REGEX=$(printf "s/%s/%s/g" '\${HOST_NAME}' "$DOMAIN_NAME")
+echo "Replacing HOST_NAME using regex: $REGEX"
 /bin/sed -i $REGEX /etc/nginx/conf.d/default.conf
 
 REGEX2=$(printf "s/%s/%s/g" '\${HOST_PORT}' "$OCTAVA_PORT")
-/bin/sed -i $REGEX2 /etc/nginx/conf.d/default.conf
+echo "Replacing HOST_PORT using regex: $REGEX2"
+#/bin/sed -i $REGEX2 /etc/nginx/conf.d/default.conf
