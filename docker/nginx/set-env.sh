74af3a2640d8d1ba27e81af6dedcb61ef2946605
Dan Chenier <daniel.chenier@shaw.ca>
2018-05-16 13:01:48 -0600

    fixing quotes

--- docker/nginx/set-env.sh
+++ docker/nginx/set-env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #parse out the domain name of OCTAVA_URL
 DOMAIN_NAME="$(echo $OCTAVA_URL | awk -F'/' '{print $3}')"
+
+#remove all quotes (if any)
+DOMAIN_NAME=$(echo "$DOMAIN_NAME" | tr -d '"')
+
 REGEX=$(printf "s/%s/%s/g" '\${HOST_NAME}' "$DOMAIN_NAME")
 
 /bin/sed -i $REGEX /etc/nginx/conf.d/default.conf
