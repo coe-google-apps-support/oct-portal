c3831a8fd77e60a36c771920c45678aad17a1ef3
Dan Chenier <daniel.chenier@shaw.ca>
2018-05-16 13:30:41 -0600

    Added PORT env to NGINX

--- docker/nginx/set-env.sh
+++ docker/nginx/set-env.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-#parse out the domain name of OCTAVA_URL
-DOMAIN_NAME="$(echo $OCTAVA_URL | awk -F'/' '{print $3}')"
 
 #remove all quotes (if any)
-DOMAIN_NAME=$(echo "$DOMAIN_NAME" | tr -d '"')
+DOMAIN_NAME=$(echo "$OCTAVA_URL" | tr -d '"')
+
+#parse out the domain name of OCTAVA_URL
+DOMAIN_NAME="$(echo $DOMAIN_NAME | awk -F'/' '{print $3}')"
+
+#get the port (if any)
+OCTAVA_PORT="$(echo $DOMAIN_NAME | awk -F':' '{print $2}')"
+
+# if no port defined use 80
+if [[ -z "${OCTAVA_PORT// }" ]] ; then OCTAVA_PORT=80; fi
+
+#now remove port from DOMAIN_NAME
+DOMAIN_NAME="$(echo $DOMAIN_NAME | awk -F':' '{print $1}')"
 
 REGEX=$(printf "s/%s/%s/g" '\${HOST_NAME}' "$DOMAIN_NAME")
-
 /bin/sed -i $REGEX /etc/nginx/conf.d/default.conf
+
+REGEX2=$(printf "s/%s/%s/g" '\${HOST_PORT}' "$OCTAVA_PORT")
+/bin/sed -i $REGEX2 /etc/nginx/conf.d/default.conf
