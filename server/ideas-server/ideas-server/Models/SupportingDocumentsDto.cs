e431d98cef287b56df7236ce9ef2e173c9d54bb0
Jackson Zhipeng Chang <zchang0302@gmail.com>
2018-05-16 22:17:31 +0000

    add new migration file to remove the busines and investment url and add addsupportingdocuments function

---
+++ server/ideas-server/ideas-server/Models/SupportingDocumentsDto.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoE.Ideas.Server.Models
+{
+	public class SupportingDocumentsDto
+	{
+		public String Title { get; set; }
+
+		public string Url { get; set; }
+
+		public int Type { get; set; }
+	}
+}
