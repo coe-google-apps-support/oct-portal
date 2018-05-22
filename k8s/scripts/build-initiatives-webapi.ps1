0cbc49444635afff1aecfac3a4fd0949f953b91f
Dan Chenier <daniel.chenier@shaw.ca>
2018-05-22 08:41:50 -0600

    syntax error.

--- k8s/scripts/build-initiatives-webapi.ps1
+++ k8s/scripts/build-initiatives-webapi.ps1
@@ -23,12 +23,12 @@ $od = Get-Content $configFileFullName | Out-String | ConvertFrom-Yaml
 $imageName = $od.spec.template.spec.containers.image.ToString()
 $tagIndex = $imageName.indexOf(":")
 if ($tagIndex -gt 0) {
-    $imageName = $imageName.SubString(0, $tagIndex) + "v" + $BUILD_BUILDID
+    $imageName = $imageName.SubString(0, $tagIndex) + ":v" + $BUILD_BUILDID
 } else {
     $imageName = $imageName + ":v" + $BUILD_BUILDID
 }
 
-$od.spec.template.spec.containers.image = $imageName
+$od.spec.template.spec.containers["image"] = $imageName
 
 Write-Host ("$configFile" + ":")
 $od.ToString()
