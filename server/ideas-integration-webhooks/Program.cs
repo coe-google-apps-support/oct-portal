c9ace4eaa9016fd163bf9838afddb651cf4cb0b9
Dan Chenier <daniel.chenier@shaw.ca>
2018-06-05 21:38:10 -0600

    re-applying changes...
    I made these changes already!

---
+++ server/ideas-integration-webhooks/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.IO;
+using System.Threading;
+
+namespace CoE.Ideas.Webhooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Configure Serilog here to ensure we capture startup errors
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            new Startup(config);
+
+            // now block forever
+            new ManualResetEvent(false).WaitOne();
+        }
+    }
+}
