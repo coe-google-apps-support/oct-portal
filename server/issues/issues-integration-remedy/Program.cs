5d23377710cee2ed471eb61b9c9a66bac87ea986
Dan Chenier <daniel.chenier@shaw.ca>
2018-06-28 14:30:57 -0600

    added issues-integration-remedy project

---
+++ server/issues/issues-integration-remedy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace issues_integration_remedy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
