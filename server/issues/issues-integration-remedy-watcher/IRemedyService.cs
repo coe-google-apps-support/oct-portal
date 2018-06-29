c12bcf6f307eaad45e10442c19294948f7f9e4e2
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-06-29 20:45:27 +0000

    Implemented a stubbed RemedyService.

---
+++ server/issues/issues-integration-remedy-watcher/IRemedyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoE.Issues.Remedy.Watcher.RemedyServiceReference;
+
+namespace CoE.Issues.Remedy.Watcher
+{
+    interface IRemedyService
+    {
+        Task<IEnumerable<OutputMapping1GetListValues>> GetRemedyChangedWorkItems(DateTime fromUtc);
+    }
+}
