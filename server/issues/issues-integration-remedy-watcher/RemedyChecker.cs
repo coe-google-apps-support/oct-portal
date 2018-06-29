1ff1ce938ce8f6a2949d863a4fd6cf1cd6e80909
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-06-29 21:15:37 +0000

    Added RemedyChecker.

---
+++ server/issues/issues-integration-remedy-watcher/RemedyChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CoE.Issues.Remedy.Watcher
+{
+    public class RemedyChecker : IRemedyChecker
+    {
+        public Task<RemedyPollResult> PollAsync(DateTime fromUtc)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ server/issues/issues-integration-remedy-watcher/RemedyCheckerOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CoE.Issues.Remedy.Watcher
+{
+    public class RemedyCheckerOptions
+    {
+        public string ServiceUserName { get; set; }
+        public string ServicePassword { get; set; }
+        public string TemplateName { get; set; }
+        public string ApiUrl { get; set; }
+        public string TempDirectory { get; set; }
+    }
+}
