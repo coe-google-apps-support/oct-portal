a0ac8ef989862252980c1c72f9e23eedcde298ef
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-07-05 01:20:03 +0000

    Added simple service bus integration tests.

---
+++ server/issues/issues-core/issues-core-tests/Integration/ServiceBus.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CoE.Issues.Core.ServiceBus;
+
+namespace CoE.Issues.Core.Tests
+{
+    class ServiceBus
+    {
+        private static ServiceProvider serviceProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.test.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceProvider = new TestConfiguration(config)
+                .ConfigureBasicServices()
+                .AddServicebus()
+                .BuildServiceProvider();
+        }
+
+        [Test]
+        public void CanConnectTopicClient()
+        {
+            Assert.DoesNotThrow(() => 
+            {
+                serviceProvider.GetRequiredService<IMessageSender>();
+            });
+        }
+
+        [Test]
+        public void CanSendMessage()
+        {
+            IDictionary<string, object> testDict = new Dictionary<string, object>();
+            testDict["one"] = "message 1";
+            testDict["two"] = "message 2";
+            testDict["three"] = "message 3";
+
+            string message = "message";
+
+            var messenger = serviceProvider.GetRequiredService<IMessageSender>();
+
+            Func<Task> asyncFunction = async () =>
+            {
+                await messenger.SendMessageAsync(message, testDict);
+                Console.WriteLine("Looks like it worked?");
+            };
+            asyncFunction.Should().NotThrow<Exception>();
+        }
+    }
+}
---
+++ server/issues/issues-core/issues-core-tests/TestConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using EnsureThat;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace CoE.Issues.Core.Tests
+{
+    public class TestConfiguration
+    {
+        public TestConfiguration(IConfigurationRoot configuration)
+        {
+            EnsureArg.IsNotNull(configuration);
+            _configuration = configuration;
+            _services = new ServiceCollection();
+        }
+
+        private readonly IConfigurationRoot _configuration;
+        private readonly IServiceCollection _services;
+
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public ServiceProvider BuildServiceProvider()
+        {
+            ServiceProvider = _services.BuildServiceProvider();
+            return ServiceProvider;
+        }
+
+        public TestConfiguration ConfigureBasicServices()
+        {
+            _services.AddOptions();
+            _services.AddMemoryCache();
+
+            // configure application specific logging
+            _services.AddSingleton<Serilog.ILogger>(x => new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Application", "issues-core-tests")
+                .Enrich.WithProperty("Module", "Server")
+                .ReadFrom.Configuration(_configuration)
+                .WriteTo.Console()
+                .CreateLogger());
+
+            return this;
+        }
+
+        internal TestConfiguration AddServicebus()
+        {
+            // Configuration["WordPress:Url"]
+            _services.AddIssueMessaging(_configuration["ServiceBus:ConnectionString"], _configuration["ServiceBus:TopicName"], _configuration["ServiceBus:Subscription"]);
+            return this;
+        }
+    }
+}
