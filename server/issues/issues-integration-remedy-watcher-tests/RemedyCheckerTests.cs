2d88ca8b9756fd711b5b86ad77e7a091722273fc
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-07-05 22:08:47 +0000

    Added RemedyCheckTests.

---
+++ server/issues/issues-integration-remedy-watcher-tests/RemedyCheckerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CoE.Issues.Remedy.Watcher.Tests
+{
+    public class RemedyCheckerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.test.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceProvider = new TestConfiguration(config)
+                .ConfigureBasicServices()
+                .BuildServiceProvider();
+        }
+
+        private static ServiceProvider serviceProvider;
+
+
+        [Test]
+        public void PollTest()
+        {
+            IRemedyChecker checker = serviceProvider.GetRequiredService<IRemedyChecker>();
+        }
+
+    }
+}
---
+++ server/issues/issues-integration-remedy-watcher-tests/TestConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using EnsureThat;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CoE.Issues.Core.Services;
+using Serilog;
+
+namespace CoE.Issues.Remedy.Watcher.Tests
+{
+    public class TestConfiguration
+    {
+        public TestConfiguration(IConfigurationRoot configuration)
+        {
+            EnsureArg.IsNotNull(configuration);
+            _configuration = configuration;
+            _services = new ServiceCollection();
+        }
+
+        private readonly IConfigurationRoot _configuration;
+        private readonly IServiceCollection _services;
+
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public ServiceProvider BuildServiceProvider()
+        {
+            ServiceProvider = _services.BuildServiceProvider();
+            return ServiceProvider;
+        }
+
+        public TestConfiguration ConfigureBasicServices()
+        {
+            _services.AddOptions();
+            _services.AddMemoryCache();
+
+            // configure application specific logging
+            _services.AddSingleton<Serilog.ILogger>(x => new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Application", "issues-integration-remedy-watcher-tests")
+                .Enrich.WithProperty("Module", "Server")
+                .ReadFrom.Configuration(_configuration)
+                .WriteTo.Console()
+                .CreateLogger());
+
+            return this;
+        }
+
+        internal TestConfiguration AddMockIssueRepository()
+        {
+            _services.AddSingleton<IIssueRepository, MockIssueRepository>();
+            return this;
+        }
+
+        internal TestConfiguration AddRemedyChecker()
+        {
+            _services.AddTransient<RemedyCheckerOptions>();
+            _services.AddSingleton<RemedyChecker>();
+            return this;
+        }
+    }
+}
