83ed3b04fba298dc6184763478f7372e8cddfc6c
Jared Rewerts <jared.rewerts@edmonton.ca>
2018-07-05 16:33:42 +0000

    Updated the issues model.

--- server/issues/issues-core/issues-core/Data/Issue.cs
+++ server/issues/issues-core/issues-core/Data/Issue.cs
@@ -1,4 +1,5 @@
 ﻿using CoE.Ideas.Shared.Data;
+using EnsureThat;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -17,13 +18,28 @@ namespace CoE.Issues.Core.Data
         {
         }
 
-        public static Issue CreateIssue(string title, string description)
+        public static Issue Create(
+            string title, 
+            string description,
+            int ownerPersonId = -1,
+            string referenceId = ""
+            )
         {
-            return new Issue()
+            Ensure.String.IsNotNullOrWhiteSpace(title, nameof(title));
+            Ensure.String.IsNotNullOrWhiteSpace(description, nameof(description));
+
+            var issue = new Issue(Guid.NewGuid())
             {
                 Title = title,
-                Description = description
+                Description = description,
+                Stakeholders = new List<Stakeholder>()
+                {
+                    Stakeholder.Create(ownerPersonId, StakeholderType.Requestor)
+                },
+                CreatedDate = DateTime.UtcNow
             };
+            
+            return issue;
         }
 
 
@@ -44,10 +60,23 @@ namespace CoE.Issues.Core.Data
         [MinLength(3)]
         public string Description { get; private set; }
 
+        /// <summary>
+        /// The date this issue was created.
+        /// </summary>
         public DateTimeOffset CreatedDate { get; private set; }
 
         /// <summary>
-        /// The person currently assigned to the initiative, usually a Business Analyst
+        /// The people that have some stake in the idea, will always include the owner
+        /// </summary>
+        public ICollection<Stakeholder> Stakeholders { get; private set; }
+
+        /// <summary>
+        /// The reference id from another system (currently Remedy).
+        /// </summary>
+        public string ReferenceId { get; private set; }
+
+        /// <summary>
+        /// The person currently assigned to the issue.
         /// </summary>
         /// <remarks>
         /// Can be null
