54bcb32862b046b0e99f162eadf4348f22c5d2ee
Dan Chenier <daniel.chenier@shaw.ca>
2018-07-16 22:28:38 -0600

    modified issues docker image entrypoint

---
+++ docker/scripts/issues-webapi-entrypoint.sh
@@ -0,0 +1,8 @@
+if [ -f /etc/secrets/appsettings.json ]; then
+  cp /etc/secrets/appsettings.json /app
+  echo "appsettings.json config specified."
+else
+  echo "No appsettings.json config specified."
+fi
+
+dotnet issues-server.dll "urls=http://0.0.0.0:5000"
